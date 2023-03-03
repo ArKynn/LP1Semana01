ea3c81741709af8bc1f7cce8c7e01816e0ab2eb9
ynnk <david.f.mendes@proton.me>
2023-03-03 13:53:14 +0000

    Added print to various numerical variables

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,7 +6,22 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            int i = 10;
+            uint ui = -10U;
+            long l = 100L;
+            ulong ul = -100UL;
+            double d = 12.345;
+            float f = 1.2345f;
+            decimal de = 0.1234m;
+
+            Console.WriteLine(i);
+            Console.WriteLine(ui);
+            Console.WriteLine(l);
+            Console.WriteLine(ul);
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+            Console.WriteLine(de);
+            
         }
     }
 }
