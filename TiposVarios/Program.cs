ee8f4b5bb81df34dd52a5993b5f56f0a7ae00ca7
ynnk <david.f.mendes@proton.me>
2023-03-05 16:15:49 +0000

    Fixed Unsolved errors and added bolean characters Print

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -7,28 +7,36 @@ namespace TiposVarios
         static void Main(string[] args)
         {
             int i = 10;
-            uint ui = -10U;
+            uint ui = 10U;
             long l = 100L;
-            ulong ul = -100UL;
-            double d = 12.345;
-            float f = 1.2345f;
-            decimal de = 0.1234m;
+            ulong ul = 100UL;
 
             Console.WriteLine(i);
             Console.WriteLine(ui);
             Console.WriteLine(l);
             Console.WriteLine(ul);
-            Console.WriteLine(d);
-            Console.WriteLine(f);
-            Console.WriteLine(de);
-            
-            char arrow1 = "\u2190";
-            char arrow2 = "\u219A";
-            char arrow3 = "\u21B0";
+
+            char arrow1 = '\u2190';
+            char arrow2 = '\u219A';
+            char arrow3 = '\u21B0';
 
             Console.WriteLine(arrow1);
             Console.WriteLine(arrow2);
             Console.WriteLine(arrow3);
+            
+            double d = 12.345;
+            float f = 1.2345f;
+            decimal de = 0.1234m;
+
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+            Console.WriteLine(de);
+
+            bool t = true;
+            bool fa = false;
+            
+            Console.WriteLine(t);
+            Console.WriteLine(fa);
         }
     }
 }
